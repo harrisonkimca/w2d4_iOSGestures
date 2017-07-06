579fa1ce44d589bde39d071f4f2f2a7cbc809dc8
hkimca <harrison.kim.ca@gmail.com>
2017-07-06 00:49:30 -0400

    Pan View

--- PanViewController.m
+++ PanViewController.m
@@ -14,24 +14,16 @@
 
 @implementation PanViewController
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view.
+- (IBAction)redViewPanned:(UIPanGestureRecognizer *)sender
+{
+    CGPoint translationInView = [sender translationInView:self.view];
+    CGPoint oldCenter = sender.view.center;
+    CGPoint newCenter = CGPointMake(oldCenter.x + translationInView.x, oldCenter.y + translationInView.y);
+    sender.view.center = newCenter;
+    // reset to zero
+    [sender setTranslation:CGPointZero inView:self.view];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
