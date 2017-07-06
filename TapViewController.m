80c738122448fd627187b711a7217b1cdcd2cacb
hkimca <harrison.kim.ca@gmail.com>
2017-07-06 01:03:46 -0400

    Tap gesture

--- TapViewController.m
+++ TapViewController.m
@@ -16,22 +16,26 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
+    // create box
+    CGFloat width = 100;
+    CGFloat height = 100;
+    CGRect frame = CGRectMake(CGRectGetMidX(self.view.bounds) - width/2, CGRectGetMidY(self.view.bounds) - height/2, width, height);
+    UIView *view = [[UIView alloc]initWithFrame:frame];
+    view.backgroundColor = [UIColor yellowColor];
+    [self.view addSubview:view];
+    
+    // add tap gesture
+    UITapGestureRecognizer *tapGesture = [[UITapGestureRecognizer alloc]initWithTarget:self action:@selector(colorChanged:)];
+    [view addGestureRecognizer:tapGesture];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)colorChanged:(UITapGestureRecognizer*)sender
+{
+    UIColor *defaultColor = sender.view.backgroundColor;
+    UIColor *newColor = [defaultColor isEqual:[UIColor yellowColor]] ? [UIColor blueColor] : [UIColor yellowColor];
+    sender.view.backgroundColor = newColor;
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
