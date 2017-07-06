04bef1e826ffc2553254448a40e4ee9906c12f85
hkimca <harrison.kim.ca@gmail.com>
2017-07-06 07:41:30 -0400

    SwipeGesture

--- PinchViewController.m
+++ PinchViewController.m
@@ -16,22 +16,25 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
+    CGFloat width = 100;
+    CGFloat height = 100;
+    CGRect frame = CGRectMake(CGRectGetMidX(self.view.bounds) - width/2, CGRectGetMidY(self.view.bounds) - height/2, width, height);
+    UIView *view = [[UIView alloc]initWithFrame:frame];
+    view.backgroundColor = [UIColor purpleColor];
+    [self.view addSubview:view];
+    
+    // add pinch gesture
+    UIPinchGestureRecognizer *pinchGesture = [[UIPinchGestureRecognizer alloc]initWithTarget:self action:@selector(viewPinched:)];
+    [view addGestureRecognizer:pinchGesture];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)viewPinched:(UIPinchGestureRecognizer*)sender
+{
+    CGFloat scale = sender.scale;
+    sender.view.transform = CGAffineTransformMakeScale(scale, scale);
+    scale = 1.0;
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
--- SwipeViewController.m
+++ SwipeViewController.m
@@ -10,28 +10,85 @@
 
 @interface SwipeViewController ()
 
+@property (nonatomic) BOOL centered;
+
 @end
 
 @implementation SwipeViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
+    CGFloat width = 380;
+    CGFloat height = 50;
+    
+    UIView *backview = [[UIView alloc]initWithFrame:CGRectMake(CGRectGetMidX(self.view.bounds) - width/2, CGRectGetMidY(self.view.bounds) - width/2, width, height)];
+    backview.backgroundColor = [UIColor greenColor];
+    [self.view addSubview:backview];
+    backview.clipsToBounds = YES;
+    
+    UIView *frontview = [[UIView alloc]initWithFrame:backview.bounds];
+    frontview.backgroundColor = [UIColor yellowColor];
+    [backview addSubview:frontview];
+    
+    // add swipe gesture
+    UISwipeGestureRecognizer *swipeGestureLeft = [[UISwipeGestureRecognizer alloc]
+                                          initWithTarget:self
+                                                  action:@selector(viewSwiped:)];
+    // add recognizer to frontview
+    swipeGestureLeft.direction = UISwipeGestureRecognizerDirectionLeft;
+    [frontview addGestureRecognizer:swipeGestureLeft];
+    
+    UISwipeGestureRecognizer *swipeGestureRight = [[UISwipeGestureRecognizer alloc]
+                                          initWithTarget:self
+                                                  action:@selector(viewSwiped:)];
+    // add recognizer to frontview
+    swipeGestureRight.direction = UISwipeGestureRecognizerDirectionRight;
+    [frontview addGestureRecognizer:swipeGestureRight];
+    
+    self.centered = YES;
+    
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)viewSwiped:(UISwipeGestureRecognizer*)sender
+{
+    // set switch for direction
+    switch (sender.direction) {
+        // left swipe to move frontView to left if swipe past center of bar
+        case UISwipeGestureRecognizerDirectionLeft:
+        {
+            if (self.centered == YES)
+            {
+                sender.view.transform = CGAffineTransformMakeTranslation(-200, 0);
+                self.centered = NO;
+            }
+            else
+            {
+                sender.view.transform = CGAffineTransformMakeTranslation(0, 0);
+                self.centered = YES;
+            }
+            break;
+        }
+            
+        // right swipe to move frontView to left if swipe past center of bar
+        case UISwipeGestureRecognizerDirectionRight:
+        {
+            if (self.centered == YES)
+            {
+                sender.view.transform = CGAffineTransformMakeTranslation(200, 0);
+                self.centered = NO;
+            }
+            else
+            {
+                sender.view.transform = CGAffineTransformMakeTranslation(0, 0);
+                self.centered = YES;
+            }
+            break;
+        }
+        default:
+            NSLog(@"Error");
+    }
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
